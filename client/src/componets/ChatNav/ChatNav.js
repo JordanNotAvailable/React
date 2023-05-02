23e5802b3e936deec3b8125b59ea936640f97dd0
unknown <jord_ogrady@hotmail.com>
2023-05-03 01:30:05 +1000

    Put buttons and search bar at the top of the page and resized

--- client/src/componets/ChatNav/ChatNav.js
+++ client/src/componets/ChatNav/ChatNav.js
@@ -6,24 +6,19 @@ export default function ChatNav() {
     <>
       <div className="container">
         <div className="header">
-          <div>
-            {/* 
-              You can add a logo here, or a Home button that redirects to the homepage.
-            */}
+          <div className="search-bar">
+            <input type="text" placeholder="Search group ID" />
           </div>
           <div className="button-group">
             <button>Create group</button>
             <button>Create chat</button>
-            <button>Add user as friend</button>
+            <button>Add friend</button>
           </div>
         </div>
         <div className="chat-list">
           <p>Chats linked to logged in user</p>
         </div>
-        <div className="search-bar">
-          <input type="text" placeholder="Search group ID" />
-        </div>
-      </div>
-    </>
+    </div>
+  </>
   )
 }
