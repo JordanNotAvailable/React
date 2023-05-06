5cb40bc820884f487407d9d0071e7aa5b6d89d13
unknown <jord_ogrady@hotmail.com>
2023-05-07 06:03:46 +1000

    Fixed styling for Header

--- client/src/componets/Info/InfoCard.js
+++ client/src/componets/Info/InfoCard.js
@@ -5,7 +5,7 @@ const InfoCard = () => {
   return (
     <div className="info-card">
       <div className="info-card-text">
-        Hi Hello this is our website. We are doing chat things and we are doing multiplayer and solo things. Please update this with proper stuff and things.
+        Welcome to TEAM CHAT! Make some friends and invite them to play some games likes Chess, Minesweeper, Connect 4 and Scissors/Paper/Rock or play alone and hvae some fun !
       </div>
     </div>
   );
--- client/src/componets/Info/InfoContainer.js
+++ client/src/componets/Info/InfoContainer.js
@@ -1,10 +1,11 @@
 import React from "react";
 import InfoCard from "./InfoCard";
+import './InfoContainer.css';
 const InfoContainer = () => {
   return (
-    <div>
+    <div className="InfoContainer">
       {" "}
-      <h1> GamesContainer </h1>
+      <h1> Welcome to the site! </h1>
       <InfoCard />
     </div>
   );
